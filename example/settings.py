6adb0064df08b9f0897c69cba283c085d4187784
Darii Denis <denis.darii@gmail.com>
2012-03-15 17:39:45 +0100

    created example project to test django-keyboard-shorcuts functionality

---
+++ example/settings.py
@@ -0,0 +1,56 @@
+import os
+PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
+os.sys.path.insert(0, os.path.dirname(PROJECT_PATH))
+
+ADMIN_MEDIA_PREFIX = '/admin_media/'
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'example.db',
+    }
+}
+DEBUG = True
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.admin',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'keyboard_shortcuts',
+)
+MEDIA_ROOT = os.path.join(PROJECT_PATH, 'media')
+MEDIA_URL = '/media'
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+)
+ROOT_URLCONF = 'example.urls'
+SECRET_KEY = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcd'
+SITE_ID = 1
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.core.context_processors.auth',
+    'django.core.context_processors.media',
+    'django.core.context_processors.request',
+)
+TEMPLATE_DEBUG = DEBUG
+TEMPLATE_DIRS = (
+        os.path.join(PROJECT_PATH, 'templates'),
+      )
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
---
+++ example/urls.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.conf.urls.defaults import *
+from django.contrib import admin
+from django.views.generic.simple import direct_to_template
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    (r'^$', direct_to_template, {'template': 'index.html'}),
+    (r'^admin/', include(admin.site.urls)),
+)
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^media/(.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT})
+    )

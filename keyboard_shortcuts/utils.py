9b3edf2fe13b77a6f62c8bc8669af6a809df950f
Darii Denis <denis.darii@gmail.com>
2012-03-20 13:20:46 +0100

    added get_combination_action functionality

--- keyboard_shortcuts/utils.py
+++ keyboard_shortcuts/utils.py
@@ -11,6 +11,18 @@ class AutoVivification(dict):
             return value
 
 
+def get_combination_action(combination):
+    """
+    Prepares the action for a keyboard combination, also filters another
+    "strange" actions declared by the user.
+    """
+    accepted_actions = ('link', 'js')
+    for action in accepted_actions:
+        if action in combination:
+            return {action: combination[action]}
+    return {}
+
+
 def get_processed_hotkeys(hotkeys=None):
     """
     Process passed dict with key combinations or the HOTKEYS dict from
@@ -24,11 +36,11 @@ def get_processed_hotkeys(hotkeys=None):
     for combination in hotkeys:
         key_codes = get_key_codes(combination['keys'])
         if len(key_codes) == 1:
-            processed_hotkeys[key_codes[0]] = {'link': combination['link']}
+            processed_hotkeys[key_codes[0]] = get_combination_action(combination)
         elif len(key_codes) == 2:
-            processed_hotkeys[key_codes[0]][key_codes[1]] = {'link': combination['link']}
+            processed_hotkeys[key_codes[0]][key_codes[1]] = get_combination_action(combination)
         elif len(key_codes) == 3:
-            processed_hotkeys[key_codes[0]][key_codes[1]][key_codes[2]] = {'link': combination['link']}
+            processed_hotkeys[key_codes[0]][key_codes[1]][key_codes[2]] = get_combination_action(combination)
         # TODO: make dynamic vivification
 
     return processed_hotkeys
@@ -36,7 +48,7 @@ def get_processed_hotkeys(hotkeys=None):
 
 def get_key_codes(keys):
     """
-    Calculate the list of key codes from a string with key combinations.
+    Calculates the list of key codes from a string with key combinations.
     Ex: 'CTRL+A' will produce the output (17, 65)
     """
     keys = keys.strip().upper().split('+')
